f37395eef734ac3eb7e0134aa2ec9849cc898711
Manuel Scholz <manuel.serrano943@gmail.com>
2025-02-12 21:06:22 +0100

    Feat: Webview

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { Dimensions, StatusBar, StyleSheet, Text, View } from 'react-native';
+import WebView from 'react-native-webview';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const ref = useRef();
+
+    return (
+        <View style={styles.container}>
+            <WebView
+                ref={ref}
+                style={styles.webview}
+                source={{ uri: 'https://carnaval-cadiz.vercel.app/' }}
+                onLoadEnd={() => ref.current.postMessage(Dimensions.get("window").height.toFixed(2))}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight + 24,
+    },
+    webview: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+    }
 });
