684ca61763dd080dca0ba2a41f4a33c2bdc9f5ed
Manuel Scholz <manuel.serrano943@gmail.com>
2026-02-12 20:51:39 +0100

    Feat: update

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import { useEffect, useRef } from 'react';
-import { Dimensions, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import WebView from 'react-native-webview';
+import Constants from "expo-constants";
 
 export default function App() {
 
     const ref = useRef();
+    const [loaded, setLoaded] = useState(false);
 
     return (
         <View style={styles.container}>
             <WebView
                 ref={ref}
-                style={styles.webview}
+                style={[{ flex: loaded ? 1 : 0 }, styles.webview]}
                 source={{ uri: 'https://carnaval-cadiz.vercel.app/' }}
-                onLoadEnd={() => ref.current.postMessage(Dimensions.get("window").height.toFixed(2))}
+                onLoadEnd={() => {
+                    setLoaded(true);
+                    ref.current.postMessage(Dimensions.get("window").height.toFixed(2))
+                }}
             />
+            { !loaded && <Text style={{ alignSelf: "center", flex: 1, fontSize: 21, fontWeight: "bold", textAlign: "center" }}>Cargando la información...</Text> }
         </View>
     );
 }
@@ -21,12 +27,11 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: StatusBar.currentHeight + 24,
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#fff',
     },
     webview: {
         alignItems: 'center',
         justifyContent: 'center',
-        flex: 1,
-        backgroundColor: '#fff',
     }
 });
